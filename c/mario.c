2df3c9651200278a8952d5a4716ff69072686c85
The-BoxHead-Guy <astralgmd@gmail.com>
2026-05-02 20:59:13 -0400

    feat: add helper functions for building wall rows and columns

--- c/mario.c
+++ c/mario.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 
 void build_wall(int n);
+void build_rows(int n, void (*function)(int));
+void build_columns(int n);
 
 int main(void) {
   // We use constants when we require the value to remain the same
@@ -10,11 +12,18 @@ int main(void) {
   build_wall(n);
 }
 
-void build_wall(int n) {
+void build_wall(int n) { build_rows(n, build_columns); }
+
+void build_rows(int n, void (*callback)(int)) {
   for (int row = 0; row < n; row++) {
-    for (int col = 0; col < n; col++) {
-      printf("#");
-    }
+    callback(n);
     printf("\n");
+  }
+}
+
+// Callback to build 'n' amount of columns
+void build_columns(int n) {
+  for (int col = 0; col < n; col++) {
+    printf("#");
   }
 }
