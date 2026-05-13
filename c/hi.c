20a10ce8df2ef659a2ea60121dff3b17b96b53bc
The-BoxHead-Guy <astralgmd@gmail.com>
2026-05-12 21:54:18 -0400

    feat: add hi program
    Adds a new program that prints greeting messages and demonstrates string
    manipulation in C.

---
+++ c/hi.c
@@ -0,0 +1,21 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void) {
+  string s = "HI!";
+  string t = "BYE!";
+
+  printf("%s\n", s);
+  printf("%s\n", t);
+
+  string words[2];
+  words[0] = "TOTALLY!";
+  words[1] = "DEFINITELY!";
+
+  printf("%s\n", words[0]);
+  printf("%s\n", words[1]);
+
+  // Strings are array of characters essentially
+  printf("%c%c%c%c\n", s[0], s[1], s[2], s[3]);
+}
+dddsss
