e5768203c1d9fdd7f60221cbd5b943a8d7a87fd7
The-BoxHead-Guy <astralgmd@gmail.com>
2026-06-20 16:12:38 -0400

    feat(cat): make loop count user-configurable
    Add functions to ask user for loop count and implement while/for loops
    that use this count instead of hardcoded values

--- python/micro-project/src/cat.py
+++ python/micro-project/src/cat.py
@@ -1,19 +1,14 @@
 def main():
     # Implementations
+
+    # Ask the user how many times the cat shall be printed
+    times = ask_user_for_times()
+
     print_header("while")
+    loop_using_while(times)
 
-    i = 0
-    while i < 3:
-        print_cat()
-        i += 1
-
-    print()
     print_header("for")
-
-    for _ in range(3):
-        print_cat()
-
-    print()
+    loop_using_for(times)
 
 # Functions
 def print_cat():
@@ -23,6 +18,20 @@ def print_cat():
 def print_header(type):
     print(f"Printing using '{type}' loop")
 
+
+def ask_user_for_times():
+    return int(input("How many times should the cat say \"Meow\"? \n"))
+
+
+def loop_using_while(times: int):
+    i = 0
+    while i < (times):
+        print_cat()
+        i += 1
+
+def loop_using_for(times):
+    for _ in range((times)):
+        print_cat()
 
 # Callers
 if __name__ == "__main__":
