73956711d83fed530765935ea94b706855ac64e4
The-BoxHead-Guy <astralgmd@gmail.com>
2026-06-20 16:49:01 -0400

    feat(phonebook): add basic phonebook lookup functionality

---
+++ python/micro-project/src/phonebook.py
@@ -0,0 +1,16 @@
+names = ["Kelly", "Jessy", "Marcos"]
+
+name = input("Name: ")
+
+# for n in names:
+#     if name == n:
+#         print("Found")
+#         break
+
+# else:
+#     print("Not found")
+
+if name in names:
+    print("Found")
+else:
+    print("Not found")
