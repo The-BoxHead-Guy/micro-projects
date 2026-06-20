005f21d18a38c317c11a07688f76eb63e6618cdf
The-BoxHead-Guy <astralgmd@gmail.com>
2026-06-20 17:17:51 -0400

    feat(phonebook): replace name list with structured people data

--- python/micro-project/src/phonebook.py
+++ python/micro-project/src/phonebook.py
@@ -1,4 +1,8 @@
-names = ["Kelly", "Jessy", "Marcos"]
+people = [
+    {"first_name": "Bill", "last_name": "Gates", "mantra": "Pivot to profit"},
+    {"first_name": "Steve", "last_name": "Jobs"},
+    {"first_name": "Tim", "last_name": "Cook"}
+]
 
 name = input("Name: ")
 
@@ -10,7 +14,16 @@ name = input("Name: ")
 # else:
 #     print("Not found")
 
-if name in names:
-    print("Found")
+for person in people:
+    if person["first_name"] == name:
+        last_name = person["last_name"]
+        mantra = person["mantra"] if "mantra" in person else None
+
+        if mantra is None:
+            print(f"Found {name} {last_name}")
+        else:
+            print(f"Found {name} {last_name}: {mantra}")
+
+        break
 else:
     print("Not found")
