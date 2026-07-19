38d4f3f556664a02f9f7d0897f1fa202806bda16
The-BoxHead-Guy <astralgmd@gmail.com>
2026-07-19 10:08:01 -0400

    feat(course): add title module with personal message and quote

---
+++ python/course/title.py
@@ -0,0 +1,18 @@
+# Personal Message
+personal_name = "   Bryan McCarthus    "
+personal_name = personal_name.strip()
+personal_message = "Hello " + personal_name
+
+print(personal_message)
+
+# Name cases
+print(personal_name.lower())
+print(personal_name.upper())
+print(personal_name.title())
+
+# Famous Quote
+quote = "Success is not final, failure is not fatal: it is the courage to continue that counts."
+quote_owner = "Winston S. Churchill"
+quote_full_message = f'{quote_owner} once said, "{quote}"'
+
+print(quote_full_message)
