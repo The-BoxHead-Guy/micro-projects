d26ebe5cdb05cf350f777c1e5b58c4cf381b9d84
The-BoxHead-Guy <astralgmd@gmail.com>
2026-07-19 10:09:13 -0400

    refactor(course/title): rename variable for clarity

--- python/course/title.py
+++ python/course/title.py
@@ -1,14 +1,14 @@
 # Personal Message
 personal_name = "   Bryan McCarthus    "
-personal_name = personal_name.strip()
-personal_message = "Hello " + personal_name
+personal_name_formatted = personal_name.strip()
+personal_message = "Hello " + personal_name_formatted
 
 print(personal_message)
 
 # Name cases
-print(personal_name.lower())
-print(personal_name.upper())
-print(personal_name.title())
+print(personal_name_formatted.lower())
+print(personal_name_formatted.upper())
+print(personal_name_formatted.title())
 
 # Famous Quote
 quote = "Success is not final, failure is not fatal: it is the courage to continue that counts."
