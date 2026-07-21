1097edeaa12b5dc6ab7b00c5bb2feba780bfec16
The-BoxHead-Guy <astralgmd@gmail.com>
2026-07-21 06:56:21 -0400

    feat(api): Add user data consumption script

---
+++ python/course/api_consumation.py
@@ -0,0 +1,10 @@
+import requests
+
+response = requests.get("https://jsonplaceholder.typicode.com/users")
+users = response.json()
+
+for user in users:
+    name = user["name"]
+    email = user["email"]
+
+    print(f"Name: {name}, Email: {email}")
