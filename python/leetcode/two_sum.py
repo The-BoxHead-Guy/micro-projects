d37a0853f555225c478b20665701a00d8f72d549
The-BoxHead-Guy <astralgmd@gmail.com>
2026-07-21 06:16:38 -0400

    feat(leetcode): implement twoSum solution skeleton
    Add initial implementation of the twoSum problem with test cases

---
+++ python/leetcode/two_sum.py
@@ -0,0 +1,33 @@
+class Solution:
+    def print_test(self):
+        print("This is a test because I don't know how to use classes in python wtf")
+
+    def sum_two_numbers(first, second) -> int:
+        return first + second
+
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        found_indexes = []
+
+        return nums
+
+
+solution = Solution()
+# print(class_instance.print_test())
+
+# Test case 1
+test_nums_1 = [2, 7, 11, 15]
+test_target_1 = 9
+
+solution.twoSum(test_nums_1, test_target_1)
+
+# Test case 2
+test_nums_2 = [3, 2, 4]
+test_target_2 = 6
+
+solution.twoSum(test_nums_2, test_target_2)
+
+# Test case 3
+test_nums_3 = [3, 3]
+test_target_3 = 6
+
+solution.twoSum(test_nums_3, test_target_3)
