5485a6fc97c39d045eac6433564a6f1dcc9d5ad7
The-BoxHead-Guy <astralgmd@gmail.com>
2026-07-21 22:06:14 -0400

    fix(two_sum): Add missing self parameter to sum_two_numbers method

--- python/leetcode/two_sum.py
+++ python/leetcode/two_sum.py
@@ -2,14 +2,34 @@ class Solution:
     def print_test(self):
         print("This is a test because I don't know how to use classes in python wtf")
 
-    def sum_two_numbers(first, second) -> int:
+    def sum_two_numbers(self, first, second) -> int:
         return first + second
 
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-        found_indexes = []
+        found_indexes: list[int] = []
 
-        return nums
+        for outer_index, outer_num in enumerate(nums):
+            for inner_index, inner_num in enumerate(nums):
+                if inner_index == outer_index:
+                    continue
 
+                inner_sum = self.sum_two_numbers(inner_num, outer_num)
+
+                if inner_sum == target:
+                    found_indexes.append(outer_index)
+                    found_indexes.append(inner_index)
+
+                    break
+
+            if found_indexes:
+                break
+
+        print(found_indexes)
+
+        return found_indexes
+
+
+# Test cases
 
 solution = Solution()
 # print(class_instance.print_test())
