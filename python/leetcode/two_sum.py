501b76c28aedd5d9cd1dc7eb0fd6cc44bbb2e4f7
The-BoxHead-Guy <astralgmd@gmail.com>
2026-07-21 22:32:36 -0400

    refactor(two_sum): simplify solution and add early return
    Remove helper method and nested loops
    Add immediate return when target is found

--- python/leetcode/two_sum.py
+++ python/leetcode/two_sum.py
@@ -1,27 +1,14 @@
 class Solution:
-    def sum_two_numbers(self, first, second) -> int:
-        return first + second
-
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-        found_indexes: list[int] = []
-
         for outer_index, outer_num in enumerate(nums):
             for inner_index, inner_num in enumerate(nums):
                 if inner_index == outer_index:
                     continue
 
-                inner_sum = self.sum_two_numbers(inner_num, outer_num)
+                if (inner_num + outer_num) == target:
+                    return [outer_index, inner_index]
 
-                if inner_sum == target:
-                    found_indexes.append(outer_index)
-                    found_indexes.append(inner_index)
-
-                    break
-
-            if found_indexes:
-                break
-
-        return found_indexes
+        return []
 
 
 # Test cases
@@ -33,22 +20,22 @@ solution = Solution()
 test_nums_1 = [2, 7, 11, 15]
 test_target_1 = 9
 
-solution.twoSum(test_nums_1, test_target_1)
+print(solution.twoSum(test_nums_1, test_target_1))
 
 # Test case 2
 test_nums_2 = [3, 2, 4]
 test_target_2 = 6
 
-solution.twoSum(test_nums_2, test_target_2)
+print(solution.twoSum(test_nums_2, test_target_2))
 
 # Test case 3
 test_nums_3 = [3, 3]
 test_target_3 = 6
 
-solution.twoSum(test_nums_3, test_target_3)
+print(solution.twoSum(test_nums_3, test_target_3))
 
 # Test case 3
 test_nums_4 = [10, 4, 3, 15, 6]
 test_target_4 = 9
 
-solution.twoSum(test_nums_4, test_target_4)
+print(solution.twoSum(test_nums_4, test_target_4))
