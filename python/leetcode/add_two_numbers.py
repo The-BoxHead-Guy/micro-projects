415eada7556b600aca3ad74cbad13635747eb42c
The-BoxHead-Guy <astralgmd@gmail.com>
2026-07-22 22:34:15 -0400

    feat(leetcode): implement addTwoNumbers solution

---
+++ python/leetcode/add_two_numbers.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def addTwoNumbers(
+        self, l1: Optional[ListNode], l2: Optional[ListNode]
+    ) -> Optional[ListNode]:
+        return list_node
+
+
+# Test case 1
+linked_list_l1 = [2, 4, 3]
+linked_list_l2 = [5, 6, 4]
+
+# Test case 2
+linked_list_ln1 = [0]
+linked_list_ln2 = [0]
+
+# Test case 3
+linked_list_lc1 = [9, 9, 9, 9, 9, 9, 9]
+linked_list_lc2 = [9, 9, 9, 9]
