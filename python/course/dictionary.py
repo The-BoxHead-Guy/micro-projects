09163722144b78495b3063ce65013acaf2fbd94b
The-BoxHead-Guy <astralgmd@gmail.com>
2026-07-26 11:27:48 -0400

    feat(dictionary): add print separator function and river/country printing

--- python/course/dictionary.py
+++ python/course/dictionary.py
@@ -1,6 +1,25 @@
+# Functions
+
+
+def print_separator():
+    print()
+
+
 rivers = {"nile": "egypt", "amazon": "brazil", "yangtze": "china", "indus": "india"}
 
 # Building a sentence with dictionaries
 for river, country in rivers.items():
     print(f"The {river.title()} runs through {country.title()}")
 
+# Building the names of whether 'contries', and 'rivers'
+print_separator()
+print("Printing the names of rivers")
+
+for river_name in rivers.keys():
+    print(river_name.title())
+
+print_separator()
+print("Printing the country in where the rivers pass through")
+
+for country_name in rivers.values():
+    print(country_name.title())
