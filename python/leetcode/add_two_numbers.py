5e9a03a71efe18d72b6a798cd57621b77da3812c
The-BoxHead-Guy <astralgmd@gmail.com>
2026-07-28 21:21:06 -0400

    refactor(leetcode/add_two_numbers): restructure solution with LinkedList class

--- python/leetcode/add_two_numbers.py
+++ python/leetcode/add_two_numbers.py
@@ -1,23 +1,78 @@
-# Definition for singly-linked list.
-from typing import Optional
-
-
 class ListNode:
-    def __init__(self, val=0, next=None):
+    def __init__(self, val, next=None):
         self.val = val
         self.next = next
+
+    def __repr__(self):
+        return self.val
+
+    def __str__(self):
+        return str(self.val)
+
+
+class LinkedList:
+    def __init__(self, nodes=None):
+        self.head = None
+
+        if nodes is not None:
+            node = ListNode(val=nodes.pop(0))
+
+            self.head = node
+
+            for element in nodes:
+                node.next = ListNode(val=element)
+                node = node.next
+
+    def __repr__(self):
+        node = self.head
+        nodes = []
+
+        while node is not None:
+            nodes.append(node.val)
+            node = node.next
+
+        return str(nodes)
+
+    def __iter__(self):
+        node = self.head
+
+        while node is not None:
+            yield node
+            node = node.next
 
 
 class Solution:
     def addTwoNumbers(
-        self, l1: Optional[ListNode], l2: Optional[ListNode]
-    ) -> Optional[ListNode]:
-        return list_node
+        self, list1: LinkedList | None, list2: LinkedList | None
+    ) -> LinkedList | None:
 
+        # We sort the linked list first
+        print(list1)
+        print(list2)
+
+        # We received the non-empty ListNode
+
+        # We sort the List Node
+
+        # We add each number
+
+        # We return the result as a Linked List or as a ListNode
+
+        # return list_node
+
+
+solution_instance = Solution()
 
 # Test case 1
-linked_list_l1 = [2, 4, 3]
-linked_list_l2 = [5, 6, 4]
+data_case_1 = [2, 4, 3]
+data_case_2 = [5, 6, 4]
+
+linked_list_1 = LinkedList(data_case_1)
+linked_list_2 = LinkedList(data_case_2)
+
+# We first need to create the Linked List, as we only have the solution and ListNode
+
+print(solution_instance.addTwoNumbers(linked_list_1, linked_list_2))
 
 # Test case 2
 linked_list_ln1 = [0]
